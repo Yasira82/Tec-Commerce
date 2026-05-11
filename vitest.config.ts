db41296aeb16c75e18d5c3486b5f4ef86eeb8bf7
Yasira82 <e23305707@gmail.com>
2026-05-11 14:23:09 +0300

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config';
-import react             from '@vitejs/plugin-react';
+import react             from '@vitejs/plugin-react-oxc';
 import * as path         from 'path';
 
 export default defineConfig({
